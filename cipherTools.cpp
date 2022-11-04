bd09bb78c2cd90fba840c8e55727fe18051b8c71
Georgesatchy <76107533+Georgesatchy@users.noreply.github.com>
2022-11-04 11:21:55 +0000

    initial commit

---
+++ cipherTools.cpp
@@ -0,0 +1,22 @@
+#include <Python.h>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+static PyMethodDef methods[] {
+    {}
+};
+
+static struct PyModuleDef cipherModule = {
+    PyModuleDef_HEAD_INIT,
+    "Cipher Tools",
+    "Cipher tools module for NEA",
+    -1,
+    methods
+};
+
+PyMODINIT_FUNC PyInit_cipherTools() {
+    printf("Cipher tools loaded!");
+    return 0;
+};
